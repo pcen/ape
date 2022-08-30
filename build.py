a3bfaee466050312b5dd1e08185213fb9791c58b
Paul Cento <paul.cento@gmail.com>
2022-08-30 01:23:13 -0400

    parallel builds

--- build.py
+++ build.py
@@ -1,23 +1,24 @@
 #!python3
 
+import multiprocessing
 import subprocess
 import sys
 
 from pathlib import Path
+from xml.etree.ElementInclude import include
 
-FLAGS = ['-std=c++17']
-SRC   = './src'
-TESTS = './test'
-BIN = './bin'
-ROOT  = Path('./')
+FLAGS    = ['-std=c++17']
+SRC      = './src'
+TESTS    = './test'
+BUILD    = './build'
+PARALLEL = False
 
 # make build directory
-def init_bin_dir():
-    Path(BIN).mkdir(exist_ok=True)
+def init_build_dir():
+    Path(BUILD).mkdir(exist_ok=True)
     for d in [SRC, TESTS]:
-        dir = Path(BIN) / d
+        dir = Path(BUILD) / d
         dir.mkdir(exist_ok=True)
-init_bin_dir()
 
 def run(cmd, *args):
     proc = subprocess.Popen(
@@ -34,6 +35,7 @@ def ts_or(path: Path, default: float = 0) -> float:
     except:
         return default
 
+
 class TU:
     def __init__(self, obj, src, incs):
         self.obj = obj
@@ -47,16 +49,12 @@ class TU:
         return self.__str__()
 
     @property
-    def target(self):
-        return f'{BIN}/{self.obj}'
-
-    @property
     def stale(self):
         return self.obj_ts <= self.src_ts or self.obj_ts <= self.max_inc_ts
 
     @property
     def obj_ts(self):
-        return ts_or(Path(self.target), 0)
+        return ts_or(Path(self.obj), 0)
 
     @property
     def src_ts(self):
@@ -66,27 +64,47 @@ class TU:
     def max_inc_ts(self):
         return max(ts_or(Path(p), 0) for p in self.incs)
 
-    def compile(self) -> bool:
+    def compile_tu(self) -> bool:
         if not self.stale:
-            print(f'{self.target} is not stale!')
+            print(f'{self.obj} is not stale!')
             return False
-        print(f'compiling {self.target} from {self.src}...')
-        _, err = run_compile(self.src, self.target)
+        print(f'compiling {self.obj} from {self.src}...')
+        _, err = run_compile(self.src, self.obj)
         if len(err) > 0:
             print(err)
             return False
         return True
 
+    def compile_binary(self, binary, dag, parallel: bool) -> bool:
+        tus = [self]
+        for h in self.incs:
+            tus.append(dag[header_to_tu(h)])
+
+        if parallel:
+            cores = multiprocessing.cpu_count()
+            pool = multiprocessing.Pool(cores)
+            pool.map(TU.compile_tu, tus)
+        else:
+            for tu in tus:
+                tu.compile_tu()
+
+        _, err = run_link(binary, [tu.obj for tu in tus])
+        if len(err) > 0:
+            print(err)
+
+def parse_dep_output(output: str):
+    obj, deps = output.split(':')
+    src, *incs = deps.split()               # source file and included headers
+    name = src.removesuffix('.cc')          # tu identifier
+    target = BUILD / Path(src).parent / obj # object output
+    return name, target.as_posix(), src, incs
+
 def get_deps(file):
     out, _ = run('g++', *FLAGS, '-MM', file)
-    obj, deps = out.split(':')
-    src, *incs = deps.split()
-    src_root = Path(src).parent
-    obj = src_root / obj.rstrip(':')
-    return obj.as_posix(), src, incs
+    return parse_dep_output(out)
 
 def get_files(dir, pattern='*'):
-    return ROOT.glob(f'{dir}/{pattern}')
+    return Path(dir).glob(f'{pattern}')
 
 def run_compile(src, obj):
     return run('g++', *FLAGS, '-c', src, f'-o{obj}')
@@ -98,37 +116,22 @@ def run_link(binary, objects):
 def create_dag(dir):
     tree = {}
     for source in get_files(dir, '*.cc'):
-        obj, src, incs = get_deps(source.as_posix())
-        tree[obj] = TU(obj, src, incs)
+        name, obj, src, incs = get_deps(source.as_posix())
+        tree[name] = TU(obj, src, incs)
     return tree
 
-# edges in the dag are represented by the header file implemented
-# by the required translation unit, so header_to_tu converts the
-# header name to the object file that identifies the translation unit
-# in the future, each translation unit should have a unique ID, such
-# as the translation unit for src/lexer.cc being 'src/lexer' instead
-# of 'src/lexer.o'
 def header_to_tu(header: str):
-    return f'{header.removesuffix(".h")}.o'
+    return header.removesuffix(".h")
 
 def build_main(binary: str):
     dag = create_dag(SRC)
     # TODO: take the target translation unit instead of hardcoding
-    main: TU = dag['src/main.o']
-    objs = []
-    for h in main.incs:
-        obj = header_to_tu(h)
-        dep_tu: TU = dag[obj]
-        dep_tu.compile()
-        objs.append(dep_tu.target)
-
-    main.compile()
-    objs.append(main.target)
-    _, err = run_link(binary, objs)
-    if len(err) > 0:
-        print(err)
+    main: TU = dag['src/main']
+    main.compile_binary(binary, dag, PARALLEL)
 
 if __name__ == '__main__':
+    init_build_dir()
+
     binary = 'ape'
     if len(sys.argv) > 1:
         binary = sys.argv[1]
