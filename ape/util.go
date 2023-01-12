86755e79534ba5a28fe32643aba4e2ea8b920b36
Zac Joffe <zacharyjoffe@gmail.com>
2023-01-12 13:13:41 -0500

    Create output directory before writing file

--- ape/util.go
+++ ape/util.go
@@ -13,11 +13,19 @@ import (
 	"github.com/pcen/ape/ape/types"
 )
 
+const OutputDir = "out"
+
 func utilWriteCode(path string, sb *strings.Builder) (string, error) {
 	base := filepath.Base(path)
 	suffix := filepath.Ext(base)
 	base = strings.TrimSuffix(base, suffix)
-	output := fmt.Sprintf("./out/%v.i", base)
+
+	if err := os.Mkdir(OutputDir, os.ModePerm); err != nil {
+		fmt.Printf("error creating output directory %v: %v\n", OutputDir, err.Error())
+		os.Exit(1)
+	}
+
+	output := fmt.Sprintf("./%v/%v.i", OutputDir, base)
 	return output, os.WriteFile(output, []byte(sb.String()), 0664)
 }
 
